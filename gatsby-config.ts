0b363495170cf2459dcd9bd02ce925b98524ed16
cozyzoey <devzoeykim@gmail.com>
2023-07-08 23:02:14 +0900

    Font Gothic A1

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -75,6 +75,22 @@ const config: GatsbyConfig = {
       },
     },
     "gatsby-plugin-root-import",
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Gothic A1`,
+            file: `https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100;200;300;400;500;600;700;800;900&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,11 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Gothic A1", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
