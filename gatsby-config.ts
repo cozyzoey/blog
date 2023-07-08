89b3efb38c54b8698d9ad6346de92b5b0e681fc3
cozyzoey <devzoeykim@gmail.com>
2023-07-08 17:56:06 +0900

    Add plugins; typescript, root import

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -66,6 +66,15 @@ const config: GatsbyConfig = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    "gatsby-plugin-root-import",
   ],
 };
 
