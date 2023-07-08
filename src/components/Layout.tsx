76cbe33e59df60c281a5fcd55ae04b468f80c5ab
cozyzoey <devzoeykim@gmail.com>
2023-07-08 18:47:20 +0900

    Layout, header, footer setup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from "react";
 import { HeadProps } from "gatsby";
+import Header from "components/header";
+import Footer from "components/footer";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="max-w-screen-lg mx-auto flex flex-col gap-y-12">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 }
 
 export const Head = ({}: HeadProps) => {
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function footer() {
+  return (
+    <footer className="pt-4 pb-6 border-solid border-t border-gray-300">
+      footer
+    </footer>
+  );
+}
