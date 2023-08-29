0e6defe17c19c22291121fbad6b361e279d91837
cozyzoey <devzoeykim@gmail.com>
2023-08-29 20:00:07 +0900

    Change header logo spacing ( padding → margin)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 export default function () {
   return (
     <header className="pt-10 pb-4 border-solid border-b border-gray-300">
-      <Link to="/" className="text-3xl font-medium block pb-5">
+      <Link to="/" className="text-3xl font-medium block mb-5">
         CozyZoey's blog
       </Link>
       <nav className="text-xl">
